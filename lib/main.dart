575c6ad0da683d9870730d5f3eecbd3a8e571aa9
unknown <abed10102000r@gmail.com>
2022-03-23 13:56:06 +0200

    start project

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+// ignore_for_file: prefer_const_constructors
+// Name : Abedulrahman Moeen ALattar
+// ID   : 20183244
+//Create AppBar to Telegram App
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+// ignore: use_key_in_widget_constructors
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeApp(),
+    );
+  }
+}
+
+// ignore: use_key_in_widget_constructors
+class HomeApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text(
+          "تيليجرام",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+            fontSize: 22.6,
+          ),
+        ),
+        // ignore: prefer_const_literals_to_create_immutables
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          )
+        ],
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
